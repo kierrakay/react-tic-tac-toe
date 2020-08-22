d054a8d02c8a02b0c10db627a6c0322455eeadd5
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-22 13:12:49 -0400

    unique key added

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ class Board extends React.Component {
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
